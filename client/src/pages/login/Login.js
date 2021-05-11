ac58b92ea89a2b5d8f6d7bd96e9ef80acf1e0c32
iliyasBr <iliyasboumour@gmail.com>
2021-05-11 00:04:26 +0000

    form login resp

--- client/src/pages/login/Login.js
+++ client/src/pages/login/Login.js
@@ -52,7 +52,7 @@ const Login = () => {
             <h2 className="text-center">{isLogin ? "Login" : "Sign Up"}</h2>
             <Form
               onSubmit={submitform}
-              className="d-flex flex-column justify-content-center w-75 mx-auto"
+              className="d-flex flex-column justify-content-center w-75 mx-auto my-4"
             >
               {isLogin || (
                 <Form.Control
