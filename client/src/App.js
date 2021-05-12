259045201496b690f0169dfc049df4fbdfa641bd
abdou-bouzzit <bouzzitabderrahman@gmail.com>
2021-05-12 23:30:11 +0000

    new commit

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,8 @@ import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 import Login from "./pages/login/Login";
 import Messages from "./pages/messages/Messages";
+import Friends from "./Components/ListFriends/Friends";
+import Images from "./Components/Images/Images";
 function App() {
   return (
     <Router>
@@ -18,6 +20,9 @@ function App() {
           <Route path="/profile" component={Profile} />
           <Route path="/login" component={Login} />
           <Route path="/messages" component={Messages} />
+          <Route path="/listFriends" component={Friends} />
+          <Route path="/Images" component={Images} />
+
         </Switch>
         <MessagesBtn />
       </StylesProvider>
