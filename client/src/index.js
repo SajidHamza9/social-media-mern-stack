092a18fbb580fdc11c265f05e133f147332168ee
iliyasBr <iliyasboumour@gmail.com>
2021-05-18 15:43:01 +0100

    connected

--- client/src/index.js
+++ client/src/index.js
@@ -7,6 +7,9 @@ import App from "./App";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 
+import { loadUser } from "./redux/actions/authActions";
+store.dispatch(loadUser());
+
 ReactDOM.render(
   <Provider store={store}>
     <App />
